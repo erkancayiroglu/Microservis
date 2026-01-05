412e3c7264208f3a635315d0a44b0d1924282653
erkancayiroglu <erkancayiroglu02@gmail.com>
2026-01-05 16:03:29 +0300

    Kategori, Ürün , Ürün Detay, Ürün Görsel Api oluşturma

---
+++ Services/Catalog/Microservis.Catalog/Controllers/ProductImagesController.cs
@@ -0,0 +1,50 @@
+﻿using Microservis.Catalog.Dtos.ProductImageDtos;
+using Microservis.Catalog.Services.ProductImageServices;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Microservis.Catalog.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductImagesController : ControllerBase
+    {
+
+        private readonly IProductImageService _ProductImageService;
+        public ProductImagesController(IProductImageService productImageService)
+        {
+            _ProductImageService = productImageService;
+        }
+        [HttpGet]
+        public async Task<IActionResult> ProductImageList()
+        {
+
+            var values = await _ProductImageService.GetAllProductImageAsync();
+            return Ok(values);
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetProductImageById(string id)
+        {
+            var values = await _ProductImageService.GetByIdProductImageAsync(id);
+            return Ok(values);
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateProductImage(CreateProductImageDto createProductImageDto)
+        {
+            await _ProductImageService.CreateProductImageAsync(createProductImageDto);
+            return Ok("Ürün Resmi başarıyla eklendi");
+        }
+        [HttpDelete]
+        public async Task<IActionResult> DeleteProductImage(string id)
+        {
+            await _ProductImageService.DeleteProductImageAsync(id);
+            return Ok("Ürün Resmi başarıyla silindi");
+        }
+        [HttpPut]
+        public async Task<IActionResult> UpdateProductImage(UpdateProductImageDto updateProductImageDto)
+        {
+            await _ProductImageService.UpdateProductImageAsync(updateProductImageDto);
+            return Ok("Ürün Resmi başarıyla güncellendi");
+        }
+    }
+}
---
+++ Services/Catalog/Microservis.Catalog/Controllers/ProductsController.cs
@@ -0,0 +1,50 @@
+﻿using Microservis.Catalog.Dtos.ProductDtos;
+using Microservis.Catalog.Services.ProductServices;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Microservis.Catalog.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+
+        private readonly IProductService _ProductService;
+        public ProductsController(IProductService productService)
+        {
+            _ProductService = productService;
+        }
+        [HttpGet]
+        public async Task<IActionResult> ProductList()
+        {
+
+            var values = await _ProductService.GetAllProductAsync();
+            return Ok(values);
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetProductById(string id)
+        {
+            var values = await _ProductService.GetByIdProductAsync(id);
+            return Ok(values);
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateProduct(CreateProductDto createProductDto)
+        {
+            await _ProductService.CreateProductAsync(createProductDto);
+            return Ok("Ürün başarıyla eklendi");
+        }
+        [HttpDelete]
+        public async Task<IActionResult> DeleteProduct(string id)
+        {
+            await _ProductService.DeleteProductAsync(id);
+            return Ok("ürün başarıyla silindi");
+        }
+        [HttpPut]
+        public async Task<IActionResult> UpdateProduct(UpdateProductDto updateProductDto)
+        {
+            await _ProductService.UpdateProductAsync(updateProductDto);
+            return Ok("ürün başarıyla güncellendi");
+        }
+    }
+}
