81ad82822011c229f405b9740cb820d71f971c95
erkancayiroglu <erkancayiroglu02@gmail.com>
2026-01-05 13:59:43 +0300

    Kategori servis sınıfı

---
+++ Services/Catalog/Microservis.Catalog/Services/CategoryServices/ICategoryService.cs
@@ -0,0 +1,17 @@
+﻿using Microservis.Catalog.Dtos.CategoryDtos;
+
+namespace Microservis.Catalog.Services.CategoryServices
+{
+    public interface ICategoryService
+    {
+        Task<List<ResultCategoryDto>> GetAllCategoryAsync();
+
+        Task CreateCategoryAsync(CreateCategoryDto createCategoryDto);
+
+        Task UpdateCategoryAsync(UpdateCategoryDto updateCategoryDto);
+
+        Task DeleteCategoryAsync(string id);
+
+        Task<GetByIdCategoryDto> GetByIdCategoryAsync(string id);
+    }
+}
