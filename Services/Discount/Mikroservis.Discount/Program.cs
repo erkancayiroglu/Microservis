096aff6c4405f418b6444881580f0165031a5e00
erkancayiroglu <erkancayiroglu02@gmail.com>
2026-01-06 14:10:40 +0300

    Discount Api Metotları

--- Services/Discount/Mikroservis.Discount/Program.cs
+++ Services/Discount/Mikroservis.Discount/Program.cs
@@ -1,9 +1,14 @@
+using Mikroservis.Discount.Context;
+using Mikroservis.Discount.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddTransient<DapperContext>();
+builder.Services.AddTransient<IDiscountService, DiscountService>();
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
